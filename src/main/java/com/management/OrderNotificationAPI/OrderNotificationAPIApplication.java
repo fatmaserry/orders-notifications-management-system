9f7f059e9ebb9f84313d50de2424f4010aebfd44
Fatma Elzahraa Ashraf <fatmaserry16@gmail.com>
2023-12-28 19:57:27 +0200

    projects files

---
+++ src/main/java/com/management/OrderNotificationAPI/OrderNotificationAPIApplication.java
@@ -0,0 +1,13 @@
+package com.management.OrderNotificationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderNotificationAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderNotificationAPIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/OrdersNotificationSystem/OrdersNotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.OrdersNotificationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersNotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
